0418bafb5eec8c0c51280867f71dd73c9555385d
NamanKhurana <khurananaman06@gmail.com>
2020-05-19 07:40:43 +0530

    2d queen combination and permutation

--- PEP_DS_ADV/Recursion/l002.cpp
+++ PEP_DS_ADV/Recursion/l002.cpp
@@ -58,7 +58,7 @@ int queenPermutation1D(vector<bool> &boxes, int qpsf, int tnq, string ans)
     return count;
 }
 
-int queenPermutation1DSub(vector<bool> &boxes,int idx,int qpsf, int tnq, string ans)
+int queenPermutation1DSub(vector<bool> &boxes, int idx, int qpsf, int tnq, string ans)
 {
 
     if (idx == boxes.size())
@@ -76,23 +76,69 @@ int queenPermutation1DSub(vector<bool> &boxes,int idx,int qpsf, int tnq, string 
     if (!boxes[idx])
     {
         boxes[idx] = true;
-        count += queenPermutation1DSub(boxes, 0, qpsf + 1, tnq, ans + "B" + to_string(idx+1) + "Q" + to_string(qpsf+1) + " ");
+        count += queenPermutation1DSub(boxes, 0, qpsf + 1, tnq, ans + "B" + to_string(idx + 1) + "Q" + to_string(qpsf + 1) + " ");
         boxes[idx] = false;
     }
     count += queenPermutation1DSub(boxes, idx + 1, qpsf, tnq, ans);
 
     return count;
+}
 
+int queenCombination2D(vector<vector< bool>> &boxes, int qpsf, int idx, int tnq, string ans)
+{
+
+    if (tnq == 0)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+    int count = 0;
+    for (int i = idx; i < boxes.size() * boxes[0].size(); i++)
+    {
+        //to convert 2-D to 1-D
+        int x = i/boxes[0].size();
+        int y = i%boxes[0].size();
+
+        count += queenCombination2D(boxes, qpsf + 1,i + 1, tnq - 1,  ans + "(" + to_string (x) + "," + to_string(y) + ") ");
+    }
+    return count;
+}
+
+int queenPermutation2D(vector<vector<bool>> &boxes,int qpsf, int tnq, string ans)
+{
+
+    if (tnq == 0)
+    {
+        cout << ans << endl;
+        return 1;
+    }
+    int count = 0;
+    for (int i = 0; i < boxes.size()*boxes[0].size(); i++)
+    {
+        int x = i/boxes[0].size();
+        int y = i%boxes[0].size();
+
+        if (!boxes[x][y])
+        {
+            boxes[x][y] = true;
+            count += queenPermutation2D(boxes, qpsf + 1, tnq - 1, ans + "(" + to_string (x) + "," + to_string(y) + ") ");
+            boxes[x][y] = false;
+        }
+    }
+    return count;
 }
 
 void queenSet()
 {
-    vector<bool> boxes(5, false);
+    // vector<bool> boxes(4, false);
+    vector<vector<bool>> boxes(4, vector<bool>(4, false));
     // cout<<queenCombination1D(boxes,0,3,0,"");
     // cout<<queenCombination1DSub(boxes,0,3,0,"");
     // cout << queenPermutation1D(boxes, 0, 3, "");
     // cout<<endl<<endl<<endl;
-    cout << queenPermutation1DSub(boxes, 0, 0,3, "");
+    // cout << queenPermutation1DSub(boxes, 0, 0,3, "");
+    // cout << queenCombination2D(boxes, 0, 0, 3, "");
+    cout << queenPermutation2D(boxes, 0,3, "");
 }
 
 void solve()

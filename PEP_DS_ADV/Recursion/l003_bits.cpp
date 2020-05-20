eadc4616a47605793298bb6142b171ab94de05f5
NamanKhurana <khurananaman06@gmail.com>
2020-05-20 09:23:54 +0530

    bits easy leetcode

--- PEP_DS_ADV/Recursion/l003_bits.cpp
+++ PEP_DS_ADV/Recursion/l003_bits.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
 int setKthBit(int n, int k)
@@ -27,6 +27,7 @@ int noOfSetBits_01(int n)
     return count;
 }
 
+//LEETCODE 191
 int noOfSetBits_02(int n)
 {
     int count = 0;
@@ -38,7 +39,7 @@ int noOfSetBits_02(int n)
     return count;
 }
 
-
+//LEETCODE 338
 vector<int> countBits(int num)
 {
     vector<int> result;
@@ -50,6 +51,76 @@ vector<int> countBits(int num)
     }
 
     return result;
+}
+
+//LEETCODE 137
+int singleNumberII(vector<int> &nums)
+{
+    int ans = 0;
+    int k = 3;
+    for (int i = 0; i < 32; i++)
+    {
+        int count = 0;
+        int mask = (1 << i);
+        for (int ele : nums)
+        {
+            if (ele & mask)
+            {
+                count++;
+            }
+        }
+        if (count % k != 0)
+        {
+            ans |= mask;
+        }
+    }
+    return ans;
+}
+
+//LEETCODE 260
+vector<int> singleNumberIII(vector<int> &nums)
+{
+    int xorNum = 0;
+    for (int ele : nums)
+        xorNum ^= ele;
+
+    int firstLSB = xorNum & (~(xorNum - 1)); //(xorNum & (-xorNum))
+    int a = 0;
+    int b = 0;
+    for (int ele : nums)
+    {
+        if ((firstLSB & ele) != 0)
+            a ^= ele;
+        else
+            b ^= ele;
+    }
+
+    return {a, b}; // java: new int[]{a,b};
+}
+
+//LEETCODE 231
+bool isPowerOfTwo(int n)
+{
+    return (n > 0 && (n & (n - 1)) == 0);
+}
+
+//LEETCODE 342
+bool isPowerOfFour(int n)
+{
+    if (n > 0 && !(n & (n - 1))) // is num power of 2
+    {
+        int count = 0; // count of all zeros after 1.
+        while (n > 1)
+        {
+            count++;
+            n >>= 1;
+        }
+
+        if ((count & 1) == 0)
+            return true; // count of zeros after 1 should be a even number.
+    }
+
+    return false;
 }
 
 int main()

91dd118ea25634effb4f70c11dffd4172f6a3471
NamanKhurana <khurananaman06@gmail.com>
2020-05-24 07:55:53 +0530

    extraRec

--- PEP_DS_ADV/Recursion/extraRec.cpp
+++ PEP_DS_ADV/Recursion/extraRec.cpp
@@ -17,6 +17,32 @@ void lexOrder(int n, int idx, int prevPart)
     }
 }
 
+int numTilePossibilities(string &str)
+{
+    if (str.length() == 0)
+    {
+        return 0;
+    }
+
+    int count = 0;
+    // vector<bool> vis(26, false);
+    int vis = 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        // int chIdx = str[i] - 'A';
+        int mask = 1 << (str[i] - 'A');
+
+        // if (vis[chIdx] == false)
+        if ((vis & mask) == 0)
+        {
+            vis ^= mask;
+            string nstr = str.substr(0, i) + str.substr(i + 1);
+            count += numTilePossibilities(nstr) + 1;
+        }
+    }
+    return count;
+}
+
 int main()
 {
     int n;

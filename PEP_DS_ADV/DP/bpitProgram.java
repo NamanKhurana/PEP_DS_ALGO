f8fe2679a731419e136ed17a881502c05d58dec5
NamanKhurana <khurananaman06@gmail.com>
2020-05-27 09:24:33 +0530

    fibo DP

--- PEP_DS_ADV/DP/bpitProgram.java
+++ PEP_DS_ADV/DP/bpitProgram.java
@@ -3,6 +3,52 @@ import java.util.ArrayList;
 
 public class bpitProgram{
     public static Scanner sc = new Scanner(System.in);
+
+    public static int fib_01(int n, int[] dp) {
+		if (n <= 1) {
+			dp[n] = n;
+			return n;
+		}
+
+		if (dp[n] != 0) return dp[n];
+
+		int ans = fib_01(n - 1, dp) + fib_01(n - 2, dp);
+
+		return dp[n] = ans;
+	}
+
+	public static int fibDP_01(int N, int[] dp) {
+		for (int n = 0; n <= N; n++) {
+
+			if (n <= 1) {
+				dp[n] = n;
+				continue;
+			}
+
+			int ans = dp[n - 1] + dp[n - 2];
+			dp[n] = ans;
+		}
+
+		return dp[N];
+	}
+
+	public static int fibDPExtend_01(int N) {
+		int a = 0;
+		int b = 1;
+		int sum = 0;
+
+		for (int n = 0; n <= N; n++) {
+			if (n <= 1) {
+				continue;
+			}
+
+			sum = a + b;
+			a = b;
+			b = sum;
+		}
+
+		return sum;
+	}
     
     public static void main(String[] args){
         

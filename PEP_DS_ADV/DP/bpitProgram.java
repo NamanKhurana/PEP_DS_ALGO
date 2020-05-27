b11fa3c8faf9f1bdc8d32d87239335a122e51600
NamanKhurana <khurananaman06@gmail.com>
2020-05-27 09:57:47 +0530

    path series dp

--- PEP_DS_ADV/DP/bpitProgram.java
+++ PEP_DS_ADV/DP/bpitProgram.java
@@ -49,6 +49,98 @@ public class bpitProgram{
 
 		return sum;
 	}
+
+    	//PathSeries.=============================================================
+
+	public static int mazePathHV_rec(int sr, int sc, int er, int ec, int[][] dp) {
+		if (sr == er && sc == ec) {
+			dp[sr][sc] = 1;
+			return 1;
+		}
+
+		if (dp[sr][sc] != 0) return dp[sr][sc];
+
+		int count = 0;
+		if (sr + 1 <= er) count += mazePathHV_rec(sr + 1, sc, er, ec, dp);
+
+		if (sr + 1 <= er && sc + 1 <= ec) count += mazePathHV_rec(sr + 1, sc + 1, er, ec, dp);
+
+		if (sc + 1 <= ec) count += mazePathHV_rec(sr, sc + 1, er, ec, dp);
+
+		return dp[sr][sc] = count;
+	}
+
+	public static int mazePathHV_DP(int sr, int sc, int er, int ec, int[][] dp) {
+
+		for (sr = er; sr >= 0; sr--) {
+			for (sc = ec; sc >= 0; sc--) {
+
+				if (sr == er && sc == ec) {
+					dp[sr][sc] = 1;
+					continue;
+				}
+
+				int count = 0;
+				if (sr + 1 <= er) count += dp[sr + 1][sc];
+
+				if (sr + 1 <= er && sc + 1 <= ec) count += dp[sr + 1][sc + 1];
+
+				if (sc + 1 <= ec) count += dp[sr][sc + 1];
+
+				dp[sr][sc] = count;
+			}
+		}
+		return dp[0][0];
+	}
+
+	public static int mazePathMulti_rec(int sr, int sc, int er, int ec, int[][] dp) {
+		if (sr == er && sc == ec) {
+			dp[sr][sc] = 1;
+			return 1;
+		}
+
+		if (dp[sr][sc] != 0) return dp[sr][sc];
+
+		int count = 0;
+		for (int jump = 1; sr + jump <= er; jump++)
+		count += mazePathMulti_rec(sr + jump, sc, er, ec, dp);
+
+		for (int jump = 1; sr + jump <= er && sc + jump <= ec; jump++)
+		count += mazePathMulti_rec(sr + jump, sc + jump, er, ec, dp);
+
+		for (int jump = 1; sc + jump <= ec; jump++)
+		count += mazePathMulti_rec(sr, sc + jump, er, ec, dp);
+
+		return dp[sr][sc] = count;
+	}
+
+	public static int mazePathMulti_DP(int sr, int sc, int er, int ec, int[][] dp) {
+
+		for (sr = er; sr >= 0; sr--) {
+			for (sc = ec; sc >= 0; sc--) {
+
+				if (sr == er && sc == ec) {
+					dp[sr][sc] = 1;
+					continue;
+				}
+
+				int count = 0;
+				for (int jump = 1; sr + jump <= er; jump++)
+				count += dp[sr + jump][sc];
+
+				for (int jump = 1; sr + jump <= er && sc + jump <= ec; jump++)
+				count += dp[sr + jump][sc + jump];
+
+				for (int jump = 1; sc + jump <= ec; jump++)
+				count += dp[sr][sc + jump];
+
+				dp[sr][sc] = count;
+			}
+		}
+
+		return dp[0][0];
+	}
+
     
     public static void main(String[] args){
         

e14446216dcca262a89cdf2b0937a9f599fe94e1
NamanKhurana <khurananaman06@gmail.com>
2020-06-09 09:15:03 +0530

    traversals

--- PEP_DS_ADV/Trees/tree.java
+++ PEP_DS_ADV/Trees/tree.java
@@ -81,6 +81,34 @@ public class l001 {
         return min_;
     }
 
+     //Traversal.============================================================
+
+    public static void preOrder(Node node) {
+        if (node == null) return;
+
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+
+    public static void inOrder(Node node) {
+        if (node == null) return;
+
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+
+    public static void postOrder(Node node) {
+        if (node == null) return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data + " ");
+
+    }
+
     public static boolean find(Node node, int data) {
         if (node == null) return false;
 
